971f60687e9474235681f13dfc4b2b125f0c7fbb
Melissa Noelle <melissamatara@gmail.com>
2016-04-08 11:02:14 -0400

    Initial commit.

---
+++ watcher.js
@@ -0,0 +1,131 @@
+var Watcher = (function(self) { return self; }(Watcher || {}));
+
+Watcher = function(self) {
+  self.config = {
+    showLogs: false,
+    onInsert: function(){},
+    onRemove: function(){},
+    onAlter: function(){},
+    intervalWatcher: function(){},
+    intervalPeriod: 3000,
+    observerConfig: {
+      attributes: true,
+      childList: true,
+      characterData: true,
+      subtree: true,
+    },
+  };
+
+  self.using = "";
+  self.interval = null;
+  self.observer = null;
+
+  self.init = function(config) {
+    self.mergeConfig(config);
+
+    if (typeof MutationObserver === 'function' || typeof MutationObserver === 'object') {
+      self.using = "MutationObserver";
+      self.setMutationObserver(self.config.onInsert, self.config.onRemove);
+    } else {
+      self.using = "MutationEvent";
+      //self.setMutationEventListener(self.config.onInsert, self.config.onRemove);
+      var chatElements = document.querySelectorAll('.enable-chat');
+      if (chatElements) {
+        for (var i = 0; i < chatElements.length; i++) {
+          var element = chatElements[i];
+          element.classList.add('chat-enabled');
+          element.classList.remove('enable-chat');
+        }
+      }
+    } /*else {
+      self.using = "Interval";
+      self.setIntervalWatcher(self.config.intervalWatcher, self.config.intervalPeriod);
+    }*/
+  }
+
+  // Mutation Observer code
+  self.setMutationObserver = function(insertFn, removeFn) {
+    self.observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        self.log(mutation.type, mutation);
+
+        switch(mutation.type) {
+          case "childList":
+            if (mutation.addedNodes.length > 0) {
+              self.log('running onInsert function (observer)');
+              self.config.onInsert(mutation);
+            }
+
+            if (mutation.removedNodes.length > 0) {
+              self.log('running onRemove function (observer)');
+              self.config.onRemove(mutation);
+            }
+            break;
+          case "attributes":
+            self.log('running onAlter function (observer)');
+            self.config.onAlter(mutation);
+            break;
+          default:
+            self.log('uncaught mutation event (observer):', mutation);
+        };
+      });
+    });
+
+    self.observer.observe(document, self.config.observerConfig);
+  };
+  
+  self.disconnectMutationObserver = function() {
+    observer.disconnect();
+  };
+
+  // Mutation event code (deprecated)
+  self.setMutationEventListener = function(insertFn, removeFn) {
+    document.addEventListener("DOMNodeInserted", function(ev) {
+      insertFn(ev);
+    }, false);
+    document.addEventListener("DOMNodeRemoved", function(ev) {
+      removeFn(ev);
+    }, false);
+  };
+
+  // Interval watcher
+  self.setIntervalWatcher = function(intervalFn, intervalPeriod) {
+    self.interval = setInterval(intervalFn, intervalPeriod);
+  };
+
+  self.clearIntervalWatcher = function() {
+    clearInterval(self.interval);
+    self.interval = false;
+  };
+
+  self.log = function(message, object) {
+    if (self.config.showLogs) {
+      console.log(message, object);
+    }
+  };
+
+  self.talk = function() {
+    console.log('config:', self.config);
+    console.log('using:', self.using);
+    console.log('observer:', self.observer);
+    console.log('interval:', self.interval);
+  };
+
+  // Doesn't work recursively atm.
+  self.mergeConfig = function(a) {
+    var c = {};
+    
+    iterateAndAssign(self.config, c);
+    iterateAndAssign(a, c);
+
+    function iterateAndAssign(x, y) {
+      Object.keys(x).forEach(function(z) {
+        y[z] = x[z];
+      });
+    }
+
+    self.config = c;
+  };
+
+  return self;
+}(Watcher || {});
